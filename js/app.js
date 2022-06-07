b9015e3735abf71b4281349a3f2bb5f572b3d6c5
Eric Wajnrajch <ericwaje03@gmail.com>
2022-06-07 14:32:48 -0300

    incluyendo tipografías

--- js/app.js
+++ js/app.js
@@ -1,31 +1,31 @@
 const menu = document.getElementById('menu');
 
 window.onscroll = function () {
-   const y = window.scrollY;
+    const y = window.scrollY;
 
-   if (y > 20) {
-      menu.style.marginTop = '0px';
-      menu.style.backgroundColor = '#176bfc';
-      menu.style.transition = 'all 0.5s ease-in-out';
-   } else {
-      menu.style.backgroundColor = 'transparent';
-      menu.style.marginTop = '0px';
-      menu.style.transition = 'all 0.5s ease-in-out';
-   }
+    if (y > 20) {
+        menu.style.marginTop = '0px';
+        menu.style.backgroundColor = '#176bfc';
+        menu.style.transition = 'all 0.5s ease-in-out';
+    } else {
+        menu.style.backgroundColor = 'transparent';
+        menu.style.marginTop = '0px';
+        menu.style.transition = 'all 0.5s ease-in-out';
+    }
 };
 
 const ancho = () => {
-   const ancho = window.screen.width;
+    const ancho = window.screen.width;
 
-   if (ancho < 600) {
-      menu.style.backgroundColor = '#176bfc';
-   } else {
-      menu.style.backgroundColor = 'transparent';
-   }
+    if (ancho < 600) {
+        menu.style.backgroundColor = '#176bfc';
+    } else {
+        menu.style.backgroundColor = 'transparent';
+    }
 };
 
 const start = () => {
-   ancho();
+    ancho();
 };
 
 window.onload = start;
